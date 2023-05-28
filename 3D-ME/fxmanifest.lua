63d16a3aaa015900cb7bff48479015d78e4e278a
STRG <134840814+STRGDEVELOPMENT@users.noreply.github.com>
2023-05-28 21:35:47 +0100

    Added Files To Github
    Finally Imported 3D-ME Files

---
+++ 3D-ME/fxmanifest.lua
@@ -0,0 +1,25 @@
+Author 'https://forum.cfx.re/u/strg/summary'
+description 'A Modern and 3rd Dimensional / Me script'
+
+fx_version 'bodacious'
+game 'gta5'
+
+ui_page "html/index.html"
+
+
+shared_scripts {
+	'config.lua'
+}
+
+client_scripts {
+ 'client.lua'
+}
+
+server_scripts {
+ 'server.lua'
+}
+
+
+files {
+ 'html/*',
+}
